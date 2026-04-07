1e6da586fdbba8954b005dfbd24dfcdc0922f1dd
lywynl <1783235753@qq.com>
2026-04-07 21:44:01 +0800

    后端: 用户注册功能

--- src/main/java/com/lyw/appgeneration/AiAppGenerationApplication.java
+++ src/main/java/com/lyw/appgeneration/AiAppGenerationApplication.java
@@ -1,9 +1,11 @@
 package com.lyw.appgeneration;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lyw.appgeneration.mapper")
 public class AiAppGenerationApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/lyw/appgeneration/model/dto/UserRegisterRequest.java
@@ -0,0 +1,22 @@
+package com.lyw.appgeneration.model.dto;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * 用户注册请求
+ */
+@Data
+public class UserRegisterRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String userAccount;
+
+    private String userPassword;
+
+    private String checkPassword;
+}
