7f29cf67b75de2fccb029fb716813f4505e4d3b5
lywynl <1783235753@qq.com>
2026-05-30 21:03:21 +0800

    feat: 新增 app_memory_summary 表/实体/Mapper(L1摘要持久层)

---
+++ src/main/java/com/lyw/appgeneration/mapper/AppMemorySummaryMapper.java
@@ -0,0 +1,13 @@
+package com.lyw.appgeneration.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.lyw.appgeneration.model.entity.AppMemorySummary;
+
+/**
+ * L1 滚动摘要 映射层。
+ *
+ * @author <a href="https://gitee.com/lywynl">lyw</a>
+ */
+public interface AppMemorySummaryMapper extends BaseMapper<AppMemorySummary> {
+
+}
